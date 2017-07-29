cc7c0072e94bd18d24b042dd64e2d9c0467285af
Artyom Yagovdik <qweirdy@gmail.com>
2017-07-30 01:54:18 +0600

    Fixed: spaces

--- content.js
+++ content.js
@@ -1,16 +1,15 @@
 let timeline = document.getElementById('timeline');
 
 setInterval(() => {
-	let bar    = document.querySelector('.js-new-tweets-bar'),
-	    top    = document.body.scrollTop,
-	    height = timeline.offsetHeight;
+    let bar    = document.querySelector('.js-new-tweets-bar'),
+        top    = document.body.scrollTop,
+        height = timeline.offsetHeight;
 
-	if (!bar) {
-		return;
-	}
+    if (!bar) {
+        return;
+    }
 
-	bar.click();
+    bar.click();
 
-	window.scrollTo(0, top + (timeline.offsetHeight - height));
+    window.scrollTo(0, top + (timeline.offsetHeight - height));
 }, 1000);
-

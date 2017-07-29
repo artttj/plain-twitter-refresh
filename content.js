9a5f1260a995f8d92860b89a7553ed85af310542
Artyom Yagovdik <qweirdy@gmail.com>
2017-07-30 01:48:18 +0600

    Fixed bug with the scrolling to the top on refresh

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-setInterval(()=>{let a=document.querySelector('.js-new-tweets-bar');a&&a.click()},1e3);
+let timeline = document.getElementById('timeline');
+
+setInterval(() => {
+	let bar    = document.querySelector('.js-new-tweets-bar'),
+	    top    = document.body.scrollTop,
+	    height = timeline.offsetHeight;
+
+	if (!bar) {
+		return;
+	}
+
+	bar.click();
+
+	window.scrollTo(0, top + (timeline.offsetHeight - height));
+}, 1000);
+

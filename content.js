f6933f77706bdefec67dd78e839c8987ee48b3a9
Artyom Yagovdik <qweirdy@gmail.com>
2017-08-01 18:51:11 +0600

    Refactored

--- content.js
+++ content.js
@@ -1,15 +1,13 @@
 let timeline = document.getElementById('timeline');
 
 setInterval(() => {
-    let bar    = document.querySelector('.js-new-items-bar-container .js-new-tweets-bar'),
-        top    = document.body.scrollTop,
-        height = timeline.offsetHeight;
+    let bar     = document.querySelector('.js-new-items-bar-container .js-new-tweets-bar'),
+        top     = document.body.scrollTop,
+        height  = timeline.offsetHeight,
+        refresh = () => {
+            bar.click();
+            window.scrollTo(0, top + (timeline.offsetHeight - height));
+        };
 
-    if (!bar) {
-        return;
-    }
-
-    bar.click();
-
-    window.scrollTo(0, top + (timeline.offsetHeight - height));
+    return bar ? refresh() : false;
 }, 1000);

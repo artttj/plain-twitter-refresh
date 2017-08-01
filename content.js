e8629017b02ffa68ea088cd8e012653b03706593
Artyom Yagovdik <qweirdy@gmail.com>
2017-08-01 14:34:30 +0600

    Getting back

--- content.js
+++ content.js
@@ -1,16 +1,15 @@
 let timeline = document.getElementById('timeline');
 
 setInterval(() => {
-    let bar = document.querySelector('.js-new-tweets-bar');
+	let bar    = document.querySelector('.js-new-tweets-bar'),
+	    top    = document.body.scrollTop,
+	    height = timeline.offsetHeight;
 
-    if (!bar) {
-        return;
-    }
+	if (!bar) {
+		return;
+	}
 
-    let top    = document.body.scrollTop,
-        height = timeline.offsetHeight;
+	bar.click();
 
-    bar.click();
-
-    window.scrollTo(0, top + (timeline.offsetHeight - height));
+	window.scrollTo(0, top + (timeline.offsetHeight - height));
 }, 1000);
